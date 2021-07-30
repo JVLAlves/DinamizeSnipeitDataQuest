41c05db38b17609f24e27a007eba9f073a6771a8
João Vítor de Luiz Alves <J.V.L.ALVES@JVL-ALVESs-MacBook-Air.local>
2021-07-30 08:45:35 -0300

    updated macosx.go: function Snipesending added

--- DataQuest/MacOS/macosx.go
+++ DataQuest/MacOS/macosx.go
@@ -182,6 +182,80 @@ func Running() {
 	return
 }
 
+//Envia os dados do computador para o inventário no Snipeit. (Essa função recebe a variavel que recebe o tipo struct criado com os dados do computador)
+func Snipesending(mac MacOSt) {
+
+	//URL da API SnipeIt
+	url := "http://10.20.1.79:8001/api/v1/hardware"
+
+	// Token de autentiucação
+	var bearer = "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiM2NlMzRhNDM0OGNjMGRkMjczMWQyMDM0ZDQ4MzRkZTZiMTQ3MGI3ODE2YWQyM2RmMjRmMzg0YzE3ZjIzOWU1N2E5ZTg2N2E0ODhlMTg5YTEiLCJpYXQiOjE2MjY0MzU0MzYsIm5iZiI6MTYyNjQzNTQzNiwiZXhwIjoyMDk5ODIxMDM1LCJzdWIiOiIzIiwic2NvcGVzIjpbXX0.JtCQ_KStz4TluCkt_6JGJLmSGVhuY6dS_3OQ7KJicm8vSgYnfh2cwzrjjgoDU92u5RN2-fMHMji_ju6a4Lm33_nyj6_qclFV9SPRtO-UqMJe7EVkPhe0bP3co-9dVKyfUmSyi7GjVeHkUcD2OGG9m_zhu7krpwzQRBNiaNR9dJwCeBEbH1O13kKQItRl_V_-DDEtFF-bTnQ3DbnlEqZxtY4we6-qjpXmIrGmOU27pH5DUXZ8-cxqlAKP1ysBz_BJRBYGN0HZqYyL2AgrTG_k9sPds2CSyqPhbTvjm7yD5IxPOAcmasJbJoAPSyZecpNSecOL7JVsjB7UFcDPTdIy6zykIqJV6Zj-3qwkg4VrAt6iGvSIPCfSPzlydwk3o0znDHisp_9IDGuSTII49kAGnGb5Kw6WWsV9xQrXBtm6R41cwVAGc47r9j8tLux5PmlXdcrSxGS1uiiaMwZSx1ZdvZlC85f5LSpKiA0qP85acTX2R_Aav4oqsx_FN-UkBuBs8ADYC-sxMDVDuokr49IkkgVY9LUfkk8-pQX4IqKZKBOHuPAT1NsalgDPOZG9pFaIQ9kmt9Qm6TkkinNIPiwcBJ2mqHXziirtvQqylfrH2MBkXAofHK_-EEkOCAsARfFT41iw7wkJwW5ijliz5SC2ZiG6HTFS9WIG88WNiRzu9qc"
+
+	//transformando em bytes a variável hw
+	hardwarePostJSON, err := json.Marshal(mac)
+	//Tratando o ocasoional erro transformação da variável em byte
+	if err != nil {
+		panic(err)
+	}
+
+	//POST REQUEST
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(hardwarePostJSON))
+
+	//Tratando o ocasoional erro do POST/REQUEST
+	if err != nil {
+		panic(err)
+	}
+
+	//adicionando os headers a autorização
+	req.Header.Add("Authorization", bearer)
+	//definindo a formatação do REQUEST
+	req.Header.Add("Content-type", "application/json")
+
+	// Send req using http Client
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error on request.\n[ERROR] -", err)
+	}
+
+	//fechando o Response após a conclusão do código
+	defer resp.Body.Close()
+
+	//lendo o RESQUEST
+	body, err := ioutil.ReadAll(resp.Body)
+	//Tratando o ocasoional erro do request
+	if err != nil {
+		log.Println("Error on parsing response.\n[ERROR] -", err)
+	}
+
+	//coventendo Body em bytes para Body em String
+	bodyString := string(body)
+	log.Print(bodyString)
+
+	// Unmarshal do resultado do response
+	response := SnipeITHardwareResponseT{}
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		log.Printf("Reading body failed: %s", err)
+		return
+	}
+
+	//Printando o Response
+	fmt.Println("Response do POST:", response)
+
+	//Animação de Apagando Junk Files
+	fmt.Print("\nApagando Junk Files")
+	for i := 0; i < 4; i++ {
+		time.Sleep(time.Second * 1)
+		fmt.Print(".")
+	}
+
+	//Apagando Junk Files
+	cmd := exec.Command("rm", "uname.out", "sysctl.out", "hostinfo.out", "diskutil.out", "sw_vers.out")
+	stdout, _ := cmd.Output()
+	fmt.Println(string(stdout))
+}
+
 //Executa o programa em sua totalidade (criando os arquivos [Create()] e lendo os arquivos [Running])
 func Executing() {
 
@@ -367,74 +441,5 @@ func Executing() {
 	fmt.Printf("MEMORIA RAM: %v\n", mac.SnipeitMema3Ria7)
 	fmt.Printf("DISCO: %v\n\n", mac.SnipeitHd9)
 
-	//URL da API SnipeIt
-	url := "http://10.20.1.79:8001/api/v1/hardware"
-
-	// Token de autentiucação
-	var bearer = "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiM2NlMzRhNDM0OGNjMGRkMjczMWQyMDM0ZDQ4MzRkZTZiMTQ3MGI3ODE2YWQyM2RmMjRmMzg0YzE3ZjIzOWU1N2E5ZTg2N2E0ODhlMTg5YTEiLCJpYXQiOjE2MjY0MzU0MzYsIm5iZiI6MTYyNjQzNTQzNiwiZXhwIjoyMDk5ODIxMDM1LCJzdWIiOiIzIiwic2NvcGVzIjpbXX0.JtCQ_KStz4TluCkt_6JGJLmSGVhuY6dS_3OQ7KJicm8vSgYnfh2cwzrjjgoDU92u5RN2-fMHMji_ju6a4Lm33_nyj6_qclFV9SPRtO-UqMJe7EVkPhe0bP3co-9dVKyfUmSyi7GjVeHkUcD2OGG9m_zhu7krpwzQRBNiaNR9dJwCeBEbH1O13kKQItRl_V_-DDEtFF-bTnQ3DbnlEqZxtY4we6-qjpXmIrGmOU27pH5DUXZ8-cxqlAKP1ysBz_BJRBYGN0HZqYyL2AgrTG_k9sPds2CSyqPhbTvjm7yD5IxPOAcmasJbJoAPSyZecpNSecOL7JVsjB7UFcDPTdIy6zykIqJV6Zj-3qwkg4VrAt6iGvSIPCfSPzlydwk3o0znDHisp_9IDGuSTII49kAGnGb5Kw6WWsV9xQrXBtm6R41cwVAGc47r9j8tLux5PmlXdcrSxGS1uiiaMwZSx1ZdvZlC85f5LSpKiA0qP85acTX2R_Aav4oqsx_FN-UkBuBs8ADYC-sxMDVDuokr49IkkgVY9LUfkk8-pQX4IqKZKBOHuPAT1NsalgDPOZG9pFaIQ9kmt9Qm6TkkinNIPiwcBJ2mqHXziirtvQqylfrH2MBkXAofHK_-EEkOCAsARfFT41iw7wkJwW5ijliz5SC2ZiG6HTFS9WIG88WNiRzu9qc"
-
-	//transformando em bytes a variável hw
-	hardwarePostJSON, err := json.Marshal(mac)
-	//Tratando o ocasoional erro transformação da variável em byte
-	if err != nil {
-		panic(err)
-	}
-
-	//POST REQUEST
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(hardwarePostJSON))
-
-	//Tratando o ocasoional erro do POST/REQUEST
-	if err != nil {
-		panic(err)
-	}
-
-	//adicionando os headers a autorização
-	req.Header.Add("Authorization", bearer)
-	//definindo a formatação do REQUEST
-	req.Header.Add("Content-type", "application/json")
-
-	// Send req using http Client
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println("Error on request.\n[ERROR] -", err)
-	}
-
-	//fechando o Response após a conclusão do código
-	defer resp.Body.Close()
-
-	//lendo o RESQUEST
-	body, err := ioutil.ReadAll(resp.Body)
-	//Tratando o ocasoional erro do request
-	if err != nil {
-		log.Println("Error on parsing response.\n[ERROR] -", err)
-	}
-
-	//coventendo Body em bytes para Body em String
-	bodyString := string(body)
-	log.Print(bodyString)
-
-	// Unmarshal do resultado do response
-	response := SnipeITHardwareResponseT{}
-	err = json.Unmarshal(body, &response)
-	if err != nil {
-		log.Printf("Reading body failed: %s", err)
-		return
-	}
-
-	//Printando o Response
-	fmt.Println("Response do POST:", response)
-
-	//Animação de Apagando Junk Files
-	fmt.Print("\nApagando Junk Files")
-	for i := 0; i < 4; i++ {
-		time.Sleep(time.Second * 1)
-		fmt.Print(".")
-	}
-
-	//Apagando Junk Files
-	cmd := exec.Command("rm", "uname.out", "sysctl.out", "hostinfo.out", "diskutil.out", "sw_vers.out")
-	stdout, _ := cmd.Output()
-	fmt.Println(string(stdout))
-
+	Snipesending(mac)
 }
