b847d366fdc8109fb5f62255165fda75a13b1413
JVLAlves <joaovitordeluizalves@gmail.com>
2021-08-03 11:39:03 -0300

    Supposedly Finished

--- DataHub/Datahub.go
+++ DataHub/Datahub.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"sync"
 
+	"github.com/JVLAlves/DinamizeSnipeitDataQuest/DataQuest/Linux"
 	"github.com/JVLAlves/DinamizeSnipeitDataQuest/DataQuest/MacOS"
 	"github.com/JVLAlves/DinamizeSnipeitDataQuest/DataQuest/Windows"
 )
@@ -387,6 +388,176 @@ func forWindows() {
 
 }
 
+func forLinux() {
+
+	Linux.MainProgram()
+
+	//This variable receive a type MacOSt even being Windows. This is because this type is universal, but was named in the MacOS Program.
+	var lin MacOS.MacOSt = MacOS.MacOSt{}
+
+	//Populando Struct MacOSt
+	lin.SnipeitCPU11 = Linux.Infos[0]
+	lin.SnipeitMema3Ria7 = Linux.Infos[1]
+	lin.SnipeitSo8 = Linux.Infos[2]
+	lin.SnipeitHostname10 = Linux.Infos[3]
+	lin.SnipeitHd9 = Linux.Infos[4]
+
+	//Entrada Personalizada
+	var IDmodelo *string = &lin.ModelID
+	var IDstatus *string = &lin.StatusID
+	var Assettag *string = &lin.AssetTag
+	var name *string = &lin.Name
+	var modeloAtivo *string = &lin.SnipeitModel12
+
+	//Input Manual: Tipo de Ativo
+	fmt.Println("Digite o Tipo de Ativo (Exemplo:Teclado/Desktop/MacBook/Leito_de_Cartão_Mesa): ")
+	fmt.Scanf("%v", IDmodelo)
+
+	//identificando o Modelo
+	switch *IDmodelo {
+	case "Leitor_de_Cartão_Mesa":
+		*IDmodelo = "1"
+	case "Leitor_de_Cartão_Porta":
+		*IDmodelo = "2"
+	case "Mouse_Sem_Fio":
+		*IDmodelo = "3"
+	case "Roteador":
+		*IDmodelo = "4"
+	case "Roteador_Wireless":
+		*IDmodelo = "5"
+	case "Notebook":
+		*IDmodelo = "6"
+	case "Celular_Samsumg":
+		*IDmodelo = "7"
+	case "Desktop":
+		*IDmodelo = "8"
+	case "Vostro":
+		*IDmodelo = "9"
+	case "Bravia":
+		*IDmodelo = "10"
+	case "Default":
+		*IDmodelo = "11"
+	case "DP720":
+		*IDmodelo = "12"
+	case "Telefone_Grandstream":
+		*IDmodelo = "13"
+	case "NP350":
+		*IDmodelo = "14"
+	case "Samsung_Default":
+		*IDmodelo = "15"
+	case "NP530":
+		*IDmodelo = "16"
+	case "NP370":
+		*IDmodelo = "17"
+	case "Ideapad":
+		*IDmodelo = "18"
+	case "P 250":
+		*IDmodelo = "19"
+	case "Inspirion":
+		*IDmodelo = "20"
+	case "Asus_X":
+		*IDmodelo = "21"
+	case "MacBook":
+		*IDmodelo = "22"
+	case "SMS":
+		*IDmodelo = "23"
+	case "OfficeJet":
+		*IDmodelo = "24"
+	case "LaserJet":
+		*IDmodelo = "25"
+	case "Asus":
+		*IDmodelo = "26"
+	case "D11":
+		*IDmodelo = "27"
+	case "XPS":
+		*IDmodelo = "28"
+	case "C3":
+		*IDmodelo = "29"
+	case "Multilaser_Desk":
+		*IDmodelo = "30"
+	case "Zelman_Desk":
+		*IDmodelo = "31"
+	case "TV_LG":
+		*IDmodelo = "32"
+	case "Braviaa":
+		*IDmodelo = "33"
+	case "AOC":
+		*IDmodelo = "34"
+	case "Dell":
+		*IDmodelo = "35"
+	case "Flantron":
+		*IDmodelo = "36"
+	case "Samsung":
+		*IDmodelo = "37"
+	default:
+		*IDmodelo = "11"
+	}
+
+	//Input Manual: Status
+	fmt.Println("Digite o Status do Ativo (Padrão: Disponível): ")
+	fmt.Scanf("%v", IDstatus)
+
+	//Identificando o Status
+	switch *IDstatus {
+	case "Disponivel":
+		*IDstatus = "5"
+	case "Indisponivel":
+		*IDstatus = "6"
+	case "Ocupado":
+		*IDstatus = "7"
+	case "Descartado":
+		*IDstatus = "4"
+	default:
+		*IDstatus = "5"
+	}
+
+	//Inputs Manuais: Asset Tag, Name, Modelo do Ativo
+	fmt.Println("Digite uma Marcação do Ativo única (Exemplo: T008921): ")
+	fmt.Scanf("%v", Assettag)
+	fmt.Println("Digite o Nome do Ativo (Exemplo: Macbook2014/Mac/Macintosh): ")
+	fmt.Scanf("%v", name)
+	fmt.Println("Digite o Modelo do Ativo (Exemplo: Air): ")
+	fmt.Scanf("%v", modeloAtivo)
+
+	//Somente alguns prints para sinalização; Sem utilidade pratica para o código.
+	fmt.Printf("\nNOME DO DISPOSITIVO: %v\n", lin.Name)
+	fmt.Printf("ASSET TAG: %v\n", lin.AssetTag)
+	fmt.Printf("TIPO DE ATIVO: %v\n", lin.ModelID)
+	fmt.Printf("MODELO DO ATIVO: %v\n", lin.SnipeitModel12)
+	fmt.Printf("STATUS: %v\n\n", lin.StatusID)
+	fmt.Printf("DESCRIÇÃO DO ATIVO\n")
+	fmt.Printf("HOSTNAME: %v\n", lin.SnipeitHostname10)
+	fmt.Printf("S.O.: %v\n", lin.SnipeitSo8)
+	fmt.Printf("CPU: %v\n", lin.SnipeitCPU11)
+	fmt.Printf("MEMORIA RAM: %v\n", lin.SnipeitMema3Ria7)
+	fmt.Printf("DISCO: %v\n\n", lin.SnipeitHd9)
+
+	fmt.Println("Você deseja enviar essas informações para o inventário Snipeit? (sim/nao)")
+	var answer string
+	fmt.Scanf("%v", &answer)
+
+	switch answer {
+	case "sim", "s":
+		MacOS.Snipesending(lin)
+	case "nao", "n":
+		fmt.Println("Você deseja apagar os arquivos criados? (sim/nao)")
+		var anotherAnswer string
+		fmt.Scanf("%v", &anotherAnswer)
+
+		switch anotherAnswer {
+		case "sim", "s":
+			wg := &sync.WaitGroup{}
+			wg.Add(1)
+			go MacOS.Clear(wg)
+			wg.Wait()
+		case "nao", "n":
+			fmt.Println("Certo. Fique à Vontade!")
+		}
+
+	}
+
+}
+
 func main() {
 
 	fmt.Println("Qual o seus sistema operacional? (MacOS/Linux/Windows)")
@@ -397,6 +568,7 @@ func main() {
 	case "MacOS", "Macos", "MacOs", "macOS", "macos", "MACOS":
 		forMacOs()
 	case "Linux", "linux", "LINUX":
+		forLinux()
 
 	case "Windows", "WINDOWS", "windows":
 		forWindows()
--- DataQuest/Linux/bugfix.go
+++ DataQuest/Linux/bugfix.go
@@ -2,12 +2,8 @@ package Linux
 
 import (
 	"bufio"
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"os/exec"
 	"regexp"
@@ -261,198 +257,13 @@ func MainProgram() {
 	//fechando o arquivo lido
 	file.Close()
 
-	/*for i := 0; i != len(Infos); i++ {
-
-		fmt.Println(Infos[i])
-	}*/
+	cmd = exec.Command("rm", "tamanhoDoHd.txt", "SO.txt", "hostname.txt", "tamanhoDoDisco.txt")
+	stdout, _ = cmd.Output()
+	fmt.Println(string(stdout))
 
 	fmt.Println(Infos)
-}
 
-func snipeSending() {
-	//Variável recebe o tipo REQUEST
-	var hw SnipeITHardwareRequestT = SnipeITHardwareRequestT{}
-
-	//Entrada Personalizada
-	var IDmodelo *string = &hw.ModelID
-	var IDstatus *string = &hw.StatusID
-	var Assettag *string = &hw.AssetTag
-	var name *string = &hw.Name
-	var modeloAtivo *string = &hw.SnipeitModel12
-
-	fmt.Println("Digite o Tipo de Ativo (Exemplo:Teclado/Desktop/MacBook): ")
-	fmt.Scanf("%v", "%v", "%v", "%v", IDmodelo)
-
-	//identificando o Modelo
-	switch *IDmodelo {
-	case "Leitor de Cartão Mesa":
-		*IDmodelo = "1"
-	case "Leitor de Cartão Porta":
-		*IDmodelo = "2"
-	case "Mouse Sem Fio":
-		*IDmodelo = "3"
-	case "Roteador":
-		*IDmodelo = "4"
-	case "Roteador Wireless":
-		*IDmodelo = "5"
-	case "Notebook":
-		*IDmodelo = "6"
-	case "Celular Samsumg":
-		*IDmodelo = "7"
-	case "Desktop":
-		*IDmodelo = "8"
-	case "Vostro":
-		*IDmodelo = "9"
-	case "Bravia":
-		*IDmodelo = "10"
-	case "Default":
-		*IDmodelo = "11"
-	case "DP720":
-		*IDmodelo = "12"
-	case "Telefone Grandstream":
-		*IDmodelo = "13"
-	case "NP350":
-		*IDmodelo = "14"
-	case "Samsung Default":
-		*IDmodelo = "15"
-	case "NP530":
-		*IDmodelo = "16"
-	case "NP370":
-		*IDmodelo = "17"
-	case "Ideapad":
-		*IDmodelo = "18"
-	case "P 250":
-		*IDmodelo = "19"
-	case "Inspirion":
-		*IDmodelo = "20"
-	case "Asus X":
-		*IDmodelo = "21"
-	case "MacBook":
-		*IDmodelo = "22"
-	case "SMS":
-		*IDmodelo = "23"
-	case "OfficeJet":
-		*IDmodelo = "24"
-	case "LaserJet":
-		*IDmodelo = "25"
-	case "Asus":
-		*IDmodelo = "26"
-	case "D11":
-		*IDmodelo = "27"
-	case "XPS":
-		*IDmodelo = "28"
-	case "C3":
-		*IDmodelo = "29"
-	case "Multilaser Desk":
-		*IDmodelo = "30"
-	case "Zelman Desk":
-		*IDmodelo = "31"
-	case "TV LG":
-		*IDmodelo = "32"
-	case "Braviaa":
-		*IDmodelo = "33"
-	case "AOC":
-		*IDmodelo = "34"
-	case "Dell":
-		*IDmodelo = "35"
-	case "Flantron":
-		*IDmodelo = "36"
-	case "Samsung":
-		*IDmodelo = "37"
-	default:
-		*IDmodelo = "11"
-	}
-
-	fmt.Println("Digite o Status do Ativo (Padrão: Disponível): ")
-	fmt.Scanf("%v", IDstatus)
-
-	//Identificando o Status
-	switch *IDstatus {
-	case "Disponível":
-		*IDstatus = "5"
-	case "Indisponível":
-		*IDstatus = "6"
-	case "Ocupado":
-		*IDstatus = "7"
-	case "Descartado":
-		*IDstatus = "4"
-	default:
-		*IDstatus = "5"
-	}
-	fmt.Println("Digite a Marcação do Ativo (Precisa ser única): ")
-	fmt.Scanf("%v", Assettag)
-	fmt.Println("Digite o Nome do Ativo: ")
-	fmt.Scanf("%v", name)
-	fmt.Println("Digite o Modelo do Ativo: ")
-	fmt.Scanf("%v", modeloAtivo)
-
-	//Populando a variável
-	hw.SnipeitCPU11 = Infos[0]
-	hw.SnipeitMema3Ria7 = Infos[1]
-	hw.SnipeitSo8 = Infos[2]
-	hw.SnipeitHostname10 = Infos[3]
-	hw.SnipeitHd9 = Infos[4]
-
-	//URL da API SnipeIt
-	url := "http://10.20.1.79:8001/api/v1/hardware"
-
-	// Token de autentiucação
-	var bearer = "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiM2NlMzRhNDM0OGNjMGRkMjczMWQyMDM0ZDQ4MzRkZTZiMTQ3MGI3ODE2YWQyM2RmMjRmMzg0YzE3ZjIzOWU1N2E5ZTg2N2E0ODhlMTg5YTEiLCJpYXQiOjE2MjY0MzU0MzYsIm5iZiI6MTYyNjQzNTQzNiwiZXhwIjoyMDk5ODIxMDM1LCJzdWIiOiIzIiwic2NvcGVzIjpbXX0.JtCQ_KStz4TluCkt_6JGJLmSGVhuY6dS_3OQ7KJicm8vSgYnfh2cwzrjjgoDU92u5RN2-fMHMji_ju6a4Lm33_nyj6_qclFV9SPRtO-UqMJe7EVkPhe0bP3co-9dVKyfUmSyi7GjVeHkUcD2OGG9m_zhu7krpwzQRBNiaNR9dJwCeBEbH1O13kKQItRl_V_-DDEtFF-bTnQ3DbnlEqZxtY4we6-qjpXmIrGmOU27pH5DUXZ8-cxqlAKP1ysBz_BJRBYGN0HZqYyL2AgrTG_k9sPds2CSyqPhbTvjm7yD5IxPOAcmasJbJoAPSyZecpNSecOL7JVsjB7UFcDPTdIy6zykIqJV6Zj-3qwkg4VrAt6iGvSIPCfSPzlydwk3o0znDHisp_9IDGuSTII49kAGnGb5Kw6WWsV9xQrXBtm6R41cwVAGc47r9j8tLux5PmlXdcrSxGS1uiiaMwZSx1ZdvZlC85f5LSpKiA0qP85acTX2R_Aav4oqsx_FN-UkBuBs8ADYC-sxMDVDuokr49IkkgVY9LUfkk8-pQX4IqKZKBOHuPAT1NsalgDPOZG9pFaIQ9kmt9Qm6TkkinNIPiwcBJ2mqHXziirtvQqylfrH2MBkXAofHK_-EEkOCAsARfFT41iw7wkJwW5ijliz5SC2ZiG6HTFS9WIG88WNiRzu9qc"
-
-	//transformando em bytes a variável hw
-	hardwarePostJSON, err := json.Marshal(hw)
-	//Tratando o ocasoional erro transformação da variável em byte
-	if err != nil {
-		panic(err)
-	}
-
-	//POST REQUEST
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(hardwarePostJSON))
-
-	//Tratando o ocasoional erro do POST/REQUEST
-	if err != nil {
-		panic(err)
-	}
-
-	//adicionando os headers a autorização
-	req.Header.Add("Authorization", bearer)
-	//definindo a formatação do REQUEST
-	req.Header.Add("Content-type", "application/json")
-
-	// Send req using http Client
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println("Error on request.\n[ERROR] -", err)
-	}
-
-	//fechando o Response após a conclusão do código
-	defer resp.Body.Close()
-
-	//lendo o RESQUEST
-	body, err := ioutil.ReadAll(resp.Body)
-	//Tratando o ocasoional erro do request
-	if err != nil {
-		log.Println("Error on parsing response.\n[ERROR] -", err)
-	}
-
-	//coventendo Body em bytes para Body em String
-	bodyString := string(body)
-	log.Print(bodyString)
-
-	// Unmarshal do resultado do response
-	response := SnipeITHardwareResponseT{}
-	err = json.Unmarshal(body, &response)
-	if err != nil {
-		log.Printf("Reading body failed: %s", err)
-		return
-	}
-
-	//Printando o Response
-	fmt.Println("Response do POST:", response)
-
-	//Apagando Junk Files
-	cmd := exec.Command("rm", "tamanhoDoHd.txt", "SO.txt", "hostname.txt", "tamanhoDoDisco.txt")
-	stdout, _ := cmd.Output()
+	cmd = exec.Command("rm", "tamanhoDoHd.txt", "SO.txt", "hostname.txt", "tamanhoDoDisco.txt")
+	stdout, _ = cmd.Output()
 	fmt.Println(string(stdout))
 }
