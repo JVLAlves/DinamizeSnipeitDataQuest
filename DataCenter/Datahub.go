3dda31cd09b1cfb5bd660a8f73cf36d33ec22ab2
JVLAlves <joaovitordeluizalves@gmail.com>
2021-08-11 10:52:08 -0300

    Clearer and Clever

--- DataCenter/Datahub.go
+++ DataCenter/Datahub.go
@@ -178,7 +178,9 @@ type IDT struct {
 	ID int `json:"id"`
 }
 
-func Getidbytag(assettag string) int {
+/*Busca o ID do Ativo Existente.
+Ele recebe o Asset Tag do Ativo Existente e retorna o ID int*/
+func Getidbytag(assettag string) (ID int) {
 	url := "http://10.20.1.79:8001/api/v1/hardware/bytag/" + assettag
 	var bearer = "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiM2NlMzRhNDM0OGNjMGRkMjczMWQyMDM0ZDQ4MzRkZTZiMTQ3MGI3ODE2YWQyM2RmMjRmMzg0YzE3ZjIzOWU1N2E5ZTg2N2E0ODhlMTg5YTEiLCJpYXQiOjE2MjY0MzU0MzYsIm5iZiI6MTYyNjQzNTQzNiwiZXhwIjoyMDk5ODIxMDM1LCJzdWIiOiIzIiwic2NvcGVzIjpbXX0.JtCQ_KStz4TluCkt_6JGJLmSGVhuY6dS_3OQ7KJicm8vSgYnfh2cwzrjjgoDU92u5RN2-fMHMji_ju6a4Lm33_nyj6_qclFV9SPRtO-UqMJe7EVkPhe0bP3co-9dVKyfUmSyi7GjVeHkUcD2OGG9m_zhu7krpwzQRBNiaNR9dJwCeBEbH1O13kKQItRl_V_-DDEtFF-bTnQ3DbnlEqZxtY4we6-qjpXmIrGmOU27pH5DUXZ8-cxqlAKP1ysBz_BJRBYGN0HZqYyL2AgrTG_k9sPds2CSyqPhbTvjm7yD5IxPOAcmasJbJoAPSyZecpNSecOL7JVsjB7UFcDPTdIy6zykIqJV6Zj-3qwkg4VrAt6iGvSIPCfSPzlydwk3o0znDHisp_9IDGuSTII49kAGnGb5Kw6WWsV9xQrXBtm6R41cwVAGc47r9j8tLux5PmlXdcrSxGS1uiiaMwZSx1ZdvZlC85f5LSpKiA0qP85acTX2R_Aav4oqsx_FN-UkBuBs8ADYC-sxMDVDuokr49IkkgVY9LUfkk8-pQX4IqKZKBOHuPAT1NsalgDPOZG9pFaIQ9kmt9Qm6TkkinNIPiwcBJ2mqHXziirtvQqylfrH2MBkXAofHK_-EEkOCAsARfFT41iw7wkJwW5ijliz5SC2ZiG6HTFS9WIG88WNiRzu9qc"
 	req, _ := http.NewRequest("GET", url, nil)
@@ -196,10 +198,6 @@ func Getidbytag(assettag string) int {
 	if err != nil {
 		log.Println("Error on parsing response.\n[ERROR] -", err)
 	}
-
-	//coventendo Body em bytes para Body em String
-	bodyString := string(body)
-	log.Print(bodyString)
 
 	// Unmarshal do resultado do response
 	response := IDT{}
@@ -214,9 +212,15 @@ func Getidbytag(assettag string) int {
 
 }
 
+//Variável Struct utilizada para a análise de disparidades entre Ativo Existente no inventário e Ativo Criado pela execução do programa
 var Analyser MacOS.MacOSt = MacOS.MacOSt{}
 
-func Getbytag(assettag string, ativo MacOS.MacOSt) (string, bool) {
+/*Busca as informações do Ativo existente no inventário Snipe it e compara com o Ativo criado ao executar o programa.
+Ele recebe o Asset Tag único do Ativo existente e a variável que contém o tipo populado com as informações do Ativo criado.
+Ao comparar ambos A. Existente e A. Criado ele destaca as disparidades e as retorna  em uma string Patchrequest, assim como um bool Needpatching que afirma se é necessário um PATCH ou não.
+
+OBS: Patchrequest é um JSON padronizado especificamente para o envio através do método PATCH.*/
+func Getbytag(assettag string, ativo MacOS.MacOSt) (Patchrequest string, Needpatching bool) {
 	url := "http://10.20.1.79:8001/api/v1/hardware/bytag/" + assettag
 	var bearer = "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiM2NlMzRhNDM0OGNjMGRkMjczMWQyMDM0ZDQ4MzRkZTZiMTQ3MGI3ODE2YWQyM2RmMjRmMzg0YzE3ZjIzOWU1N2E5ZTg2N2E0ODhlMTg5YTEiLCJpYXQiOjE2MjY0MzU0MzYsIm5iZiI6MTYyNjQzNTQzNiwiZXhwIjoyMDk5ODIxMDM1LCJzdWIiOiIzIiwic2NvcGVzIjpbXX0.JtCQ_KStz4TluCkt_6JGJLmSGVhuY6dS_3OQ7KJicm8vSgYnfh2cwzrjjgoDU92u5RN2-fMHMji_ju6a4Lm33_nyj6_qclFV9SPRtO-UqMJe7EVkPhe0bP3co-9dVKyfUmSyi7GjVeHkUcD2OGG9m_zhu7krpwzQRBNiaNR9dJwCeBEbH1O13kKQItRl_V_-DDEtFF-bTnQ3DbnlEqZxtY4we6-qjpXmIrGmOU27pH5DUXZ8-cxqlAKP1ysBz_BJRBYGN0HZqYyL2AgrTG_k9sPds2CSyqPhbTvjm7yD5IxPOAcmasJbJoAPSyZecpNSecOL7JVsjB7UFcDPTdIy6zykIqJV6Zj-3qwkg4VrAt6iGvSIPCfSPzlydwk3o0znDHisp_9IDGuSTII49kAGnGb5Kw6WWsV9xQrXBtm6R41cwVAGc47r9j8tLux5PmlXdcrSxGS1uiiaMwZSx1ZdvZlC85f5LSpKiA0qP85acTX2R_Aav4oqsx_FN-UkBuBs8ADYC-sxMDVDuokr49IkkgVY9LUfkk8-pQX4IqKZKBOHuPAT1NsalgDPOZG9pFaIQ9kmt9Qm6TkkinNIPiwcBJ2mqHXziirtvQqylfrH2MBkXAofHK_-EEkOCAsARfFT41iw7wkJwW5ijliz5SC2ZiG6HTFS9WIG88WNiRzu9qc"
 	req, _ := http.NewRequest("GET", url, nil)
@@ -235,20 +239,18 @@ func Getbytag(assettag string, ativo MacOS.MacOSt) (string, bool) {
 		log.Println("Error on parsing response.\n[ERROR] -", err)
 	}
 
-	//coventendo Body em bytes para Body em String
-	bodyString := string(body)
-	log.Print("Billy Lowkão *easteregg*")
-	log.Print(bodyString)
+	//log.Println("\nBilly Lowkão *easteregg*")
+
+	//Variavel que contém os dados do Ativo Existente
+	var responsevar UniversalGetT
 
 	// Unmarshal do resultado do response
-	var responsevar UniversalGetT //= MacOS.SnipeITHardwareResponseT{}
-	//responsevar =
-
 	err = json.Unmarshal(body, &responsevar)
 	if err != nil {
 		log.Printf("Reading body failed: %s", err)
 	}
 
+	//Armazena as informações selecioandas do Response na variável Struct de análise
 	Analyser.Name = responsevar.Name
 	Analyser.AssetTag = responsevar.AssetTag
 	Analyser.ModelID = strconv.Itoa(responsevar.Model.ID)
@@ -260,14 +262,19 @@ func Getbytag(assettag string, ativo MacOS.MacOSt) (string, bool) {
 	Analyser.SnipeitCPU11 = responsevar.CustomFields.CPU.Value
 	Analyser.SnipeitModel12 = responsevar.CustomFields.Modelo.Value
 
+	//Variável Array com as informacões da Variável Struct de análise
 	var AnalyserIndex = []string{Analyser.Name, Analyser.AssetTag, Analyser.ModelID, Analyser.StatusID, Analyser.SnipeitMema3Ria7, Analyser.SnipeitSo8, Analyser.SnipeitHd9, Analyser.SnipeitHostname10, Analyser.SnipeitCPU11, Analyser.SnipeitModel12}
 
+	//Variável Array com as informacões da Variável Struct do Ativo Criado
 	var AtivoIndex = []string{ativo.Name, ativo.AssetTag, ativo.ModelID, ativo.StatusID, ativo.SnipeitMema3Ria7, ativo.SnipeitSo8, ativo.SnipeitHd9, ativo.SnipeitHostname10, ativo.SnipeitCPU11, ativo.SnipeitModel12}
 
+	//Variavél Array que contém as alterações pendentes
 	var Pending []string
 
+	//Variável String que contém o príncipio do Patchrequest
 	var Patchresquest string = "{\"requestable\":false,\"archived\":false"
 
+	//Verifica as disparidades, destacando-as e criando o Patchrequest.
 	if Analyser != ativo {
 		fmt.Println("Disparidades encontradas!")
 		fmt.Printf("Aprofundando análises")
@@ -317,6 +324,7 @@ func Getbytag(assettag string, ativo MacOS.MacOSt) (string, bool) {
 	}
 }
 
+//Envia alterações feitas no ativo existente no inventário através de seu ID.
 func Patchbyid(id int, Patchresquest string) {
 	strid := strconv.Itoa(id)
 	url := "http://10.20.1.79:8001/api/v1/hardware/" + strid
@@ -340,10 +348,6 @@ func Patchbyid(id int, Patchresquest string) {
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
 
-	//coventendo Body em bytes para Body em String
-	bodyString := string(body)
-	log.Print(bodyString)
-
 	// Unmarshal do resultado do response
 	response := PatchRespose{}
 	err = json.Unmarshal(body, &response)
@@ -353,6 +357,7 @@ func Patchbyid(id int, Patchresquest string) {
 
 }
 
+//Verifica se ativo existe procurando-o (GET) no inventário através do seu Asset Tag único.
 func Verifybytag(assettag string) bool {
 
 	url := "http://10.20.1.79:8001/api/v1/hardware/bytag/" + assettag
@@ -369,10 +374,6 @@ func Verifybytag(assettag string) bool {
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
 
-	//coventendo Body em bytes para Body em String
-	bodyString := string(body)
-	log.Print(bodyString)
-
 	// Unmarshal do resultado do response
 	response := ErrorT{}
 	err = json.Unmarshal(body, &response)
@@ -380,6 +381,7 @@ func Verifybytag(assettag string) bool {
 		log.Printf("Reading body failed: %s", err)
 	}
 
+	//tipo vazio para a comparação. (Se a Reponse for igual a ele, isto é, vazio, então ele retorna um false significando que não há erro)
 	blankspace := ErrorT{}
 	//Printando o Response
 	if response == blankspace {
@@ -390,6 +392,7 @@ func Verifybytag(assettag string) bool {
 	}
 }
 
+//Função de execução do programa em MacOS
 func forMacOs() {
 
 	//Criando Arquivos via Goroutines
@@ -452,26 +455,24 @@ func forMacOs() {
 	var modeloAtivo *string = &mac.SnipeitModel12
 
 	//Input Manual: Tipo de Ativo
-	fmt.Println("Digite o Tipo de Ativo (Exemplo:Desktop/MacBook/Notebook): ")
+	fmt.Println("Digite o Tipo de Ativo (Exemplo:Desktop/MacBook): ")
 	fmt.Scanf("%v", IDmodelo)
 
 	//identificando o Modelo
 	switch *IDmodelo {
-	case "Notebook":
-		*IDmodelo = "6"
+
 	case "Desktop":
 		*IDmodelo = "8"
+		*modeloAtivo = "DNZ-Desktop"
 	case "MacBook":
 		*IDmodelo = "22"
+		*modeloAtivo = "DNZ-Macbook"
 	default:
 		*IDmodelo = "11"
 	}
 
 	//Status ID
 	*IDstatus = "5"
-
-	//Inputs Manuais: Modelo do Ativo
-	*modeloAtivo = "DNZ-COMPUTER"
 
 	//Somente alguns prints para sinalização; Sem utilidade pratica para o código.
 	fmt.Printf("\nNOME DO DISPOSITIVO: %v\n", mac.Name)
@@ -486,6 +487,7 @@ func forMacOs() {
 	fmt.Printf("MEMORIA RAM: %v\n", mac.SnipeitMema3Ria7)
 	fmt.Printf("DISCO: %v\n\n", mac.SnipeitHd9)
 
+	//Verificando a existência de um ativo semelhante no inventário Snipe it
 	if Verifybytag(mac.AssetTag) {
 		fmt.Println("Esse ativo ainda não consta no inventário.\nVocê deseja enviar essas informações para o inventário Snipeit? (sim/nao)")
 		var answer string
@@ -519,23 +521,24 @@ func forMacOs() {
 		}
 		patch, boolean := Getbytag(mac.AssetTag, mac)
 		if boolean {
-			fmt.Println("PATCH necessário.")
-			fmt.Println("Executando PATCH RESQUEST.")
+			fmt.Println("\nPATCH necessário.")
+			fmt.Println("\nExecutando PATCH RESQUEST.")
 			time.Sleep(time.Second * 3)
 			id := Getidbytag(mac.AssetTag)
 			Patchbyid(id, patch)
 		} else {
-			fmt.Println("Sem alterações")
+			fmt.Println("\nSem alterações")
 		}
 	}
 
 }
 
+//Função de execução do programa em Windows
 func forWindows() {
 
 	Windows.MainProgram()
 
-	//This variable receive a type MacOSt even being Windows. This is because this type is universal, but was named in the MacOS Program.
+	//Essa variavel recebe um Tipo MacOSt, pois é o contrato padrão para a execução do programa
 	var win MacOS.MacOSt = MacOS.MacOSt{}
 
 	//Populando Struct MacOSt
@@ -553,27 +556,23 @@ func forWindows() {
 	var modeloAtivo *string = &win.SnipeitModel12
 
 	//Input Manual: Tipo de Ativo
-	fmt.Println("Digite o Tipo de Ativo (Exemplo:Desktop/MacBook/Notebook): ")
+	fmt.Println("Digite o Tipo de Ativo (Exemplo:Desktop/Notebook): ")
 	fmt.Scanf("%v", IDmodelo)
 
 	//identificando o Modelo
 	switch *IDmodelo {
 	case "Notebook":
 		*IDmodelo = "6"
+		*modeloAtivo = "DNZ-Notebook"
 	case "Desktop":
 		*IDmodelo = "8"
-	case "MacBook":
-		*IDmodelo = "22"
+		*modeloAtivo = "DNZ-Desktop"
 	default:
 		*IDmodelo = "11"
 	}
 
 	//Status ID
 	*IDstatus = "5"
-
-	//Inputs Manuais: Asset Tag, Name, Modelo do Ativo
-	fmt.Println("Digite o Modelo do Ativo (Exemplo: Air): ")
-	fmt.Scanf("%v", modeloAtivo)
 
 	//Somente alguns prints para sinalização; Sem utilidade pratica para o código.
 	fmt.Printf("\nNOME DO DISPOSITIVO: %v\n", win.Name)
@@ -588,6 +587,7 @@ func forWindows() {
 	fmt.Printf("MEMORIA RAM: %v\n", win.SnipeitMema3Ria7)
 	fmt.Printf("DISCO: %v\n\n", win.SnipeitHd9)
 
+	//Verificando a existência de um ativo semelhante no inventário Snipe it
 	if Verifybytag(win.AssetTag) {
 		fmt.Println("Esse ativo ainda não consta no inventário.\nVocê deseja enviar essas informações para o inventário Snipeit? (sim/nao)")
 		var answer string
@@ -621,24 +621,26 @@ func forWindows() {
 		}
 		patch, boolean := Getbytag(win.AssetTag, win)
 		if boolean {
-			fmt.Println("PATCH necessário.")
-			fmt.Println("Executando PATCH RESQUEST.")
+			fmt.Println("\nPATCH necessário.")
+			fmt.Println("\nExecutando PATCH RESQUEST.")
 			time.Sleep(time.Second * 3)
 			id := Getidbytag(win.AssetTag)
 			Patchbyid(id, patch)
 
 		} else {
-			fmt.Println("Sem alterações")
+			fmt.Println("\nSem alterações")
 		}
 	}
 
 }
 
+//Função de execução do programa em Linux
 func forLinux() {
 
+	//programa principal para a coleta de informações em Linux
 	Linux.MainProgram()
 
-	//This variable receive a type MacOSt even being Windows. This is because this type is universal, but was named in the MacOS Program.
+	//Essa variavel recebe um Tipo MacOSt, pois é o contrato padrão para a execução do programa
 	var lin MacOS.MacOSt = MacOS.MacOSt{}
 
 	//Populando Struct MacOSt
@@ -656,7 +658,7 @@ func forLinux() {
 	var modeloAtivo *string = &lin.SnipeitModel12
 
 	//Input Manual: Tipo de Ativo
-	fmt.Println("Digite o Tipo de Ativo (Exemplo:Desktop/MacBook/Notebook): ")
+	fmt.Println("Digite o Tipo de Ativo (Exemplo:Desktop/Notebook): ")
 	fmt.Scanf("%v", IDmodelo)
 
 	//identificando o Modelo
@@ -667,9 +669,6 @@ func forLinux() {
 	case "Desktop":
 		*IDmodelo = "8"
 		*modeloAtivo = "DNZ-Desktop"
-	case "MacBook":
-		*IDmodelo = "22"
-		*modeloAtivo = "DNZ-Macbook"
 	default:
 		*IDmodelo = "11"
 	}
@@ -690,6 +689,7 @@ func forLinux() {
 	fmt.Printf("MEMORIA RAM: %v\n", lin.SnipeitMema3Ria7)
 	fmt.Printf("DISCO: %v\n\n", lin.SnipeitHd9)
 
+	//Verificando a existência de um ativo semelhante no inventário Snipe it
 	if Verifybytag(lin.AssetTag) {
 		fmt.Println("Esse ativo ainda não consta no inventário.\nVocê deseja enviar essas informações para o inventário Snipeit? (sim/nao)")
 		var answer string
@@ -723,27 +723,30 @@ func forLinux() {
 		}
 		patch, boolean := Getbytag(lin.AssetTag, lin)
 		if boolean {
-			fmt.Println("PATCH necessário.")
-			fmt.Println("Executando PATCH RESQUEST.")
+			fmt.Println("\nPATCH necessário.")
+			fmt.Println("\nExecutando PATCH RESQUEST.")
 			time.Sleep(time.Second * 3)
 			id := Getidbytag(lin.AssetTag)
 			Patchbyid(id, patch)
 
 		} else {
-			fmt.Println("Sem alterações")
+			fmt.Println("\nSem alterações")
 		}
 	}
 
 }
 
+//função principal
 func main() {
 
+	//mensagem de abertura
 	fmt.Print("Dectecting your Operating System")
 	for i := 0; i < 4; i++ {
 		time.Sleep(time.Second * 1)
 		fmt.Print(".")
 	}
 
+	//Identificando sistema operacional
 	switch runtime.GOOS {
 	case "darwin":
 		forMacOs()
@@ -763,5 +766,8 @@ func main() {
 		time.Sleep(time.Second * 3)
 		log.Fatal()
 	}
+
+	//mensagem de encerramento
+	fmt.Println("\n\nObrigado pela paciência! (FIM)")
 
 }
