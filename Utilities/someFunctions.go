c6aeff4211084f9bb213d2960a1e91c8abe60570
JVLAlves <joaovitordeluizalves@gmail.com>
2021-08-12 09:40:10 -0300

    Utilities

---
+++ Utilities/someFunctions.go
@@ -0,0 +1,20 @@
+package functions
+
+import (
+	"strconv"
+	"time"
+)
+
+func Today() (Daytime string) {
+
+	years, month, day := time.Now().Date()
+
+	Day := strconv.Itoa(day)
+	Month := strconv.Itoa(int(month))
+	Year := strconv.Itoa(years)
+
+	Daytime = "_" + Day + "_" + Month + "_" + Year + "_"
+
+	return Daytime
+
+}
